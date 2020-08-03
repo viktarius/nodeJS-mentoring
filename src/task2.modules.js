e00c6a6bbc73ac0e41b2d1bfa0e4c6344b175c09
Viktar Belski <belski.viktor.dev@gmail.com>
2020-08-03 16:51:28 +0300

    fixes

--- src/task2.modules.js
+++
@@ -1,6 +0,0 @@
-import fs from "fs";
-
-export const csvFilePath = './csv/data.csv';
-export const readStream = fs.createReadStream(csvFilePath);
-export const writeStreamTxt = fs.createWriteStream('./csv/data_RAM.txt', {flags: 'w'});
-export const writeJsonSync = (content) => fs.writeFileSync('./csv/data.json', content, {flag: 'w'});
