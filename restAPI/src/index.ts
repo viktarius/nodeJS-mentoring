ea378b4a09c62f2761dd1f020811732a29a3d290
Viktar Belski <belski.viktor.dev@gmail.com>
2020-08-17 22:51:35 +0300

    fixes

--- restAPI/src/index.ts
+++ restAPI/src/index.ts
@@ -1,6 +1,14 @@
 import express from "express";
-import { activeUsers, addUser, getAutoSuggestUsers, getCurrentUser, green } from "./utils/common";
-import { User } from "./utils/user";
+import {
+    addUser,
+    createUser,
+    deleteUser,
+    getActiveUsers,
+    getAutoSuggestUsers,
+    getCurrentUser,
+    green,
+    updateUser
+} from "./utils/common";
 import bodyParser from 'body-parser';
 import { validateUser } from "./utils/validator";
 
@@ -11,13 +19,13 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
 
 app.get('/suggest-users', (req, res) => {
-    const limit = 3;
-    const loginSubstring = "fi";
-    res.json(getAutoSuggestUsers(loginSubstring, limit));
+    const limit = req.query.limit;
+    const loginSubstring = req.query.loginSubstr;
+    res.json(getAutoSuggestUsers(String(loginSubstring), Number(limit)));
 });
 
 app.get('/users', (req, res) => {
-    res.json(activeUsers);
+    res.json(getActiveUsers());
 });
 
 app.get('/user/:id', (req, res) => {
@@ -26,23 +34,21 @@ app.get('/user/:id', (req, res) => {
     if (user) {
         res.json(user);
     }
-    res.send('USER NOT FOUND');
+    res.status(404).send('user not found');
 });
 
 app.post('/user', (req, res) => {
     try {
-        const {login, password, age} = req.body;
-        const newUser = new User(login, password, age);
+        const newUser = createUser(req.body);
         const {error} = validateUser(newUser);
-        if(error){
+        if (error) {
             res.status(400).send(error?.message)
         }
         addUser(newUser);
-        res.send('added successfully');
+        res.status(201).json(newUser);
     } catch (e) {
-        res.status(400).send('invalid user')
+        res.status(400).send(`invalid values ${e.message}`)
     }
-
 });
 
 app.put('/user/:id', (req, res) => {
@@ -50,26 +56,23 @@ app.put('/user/:id', (req, res) => {
     const user = getCurrentUser(id);
     if (user) {
         const {error} = validateUser(user);
-        if(error){
+        if (error) {
             res.status(400).send(error?.message)
         }
-        const {login, password, age} = req.body;
-        user.login = login || user.login;
-        user.password = password || user.password;
-        user.age = age || user.age;
-        res.send(`user ${user.id} successfully updated`);
+        updateUser(user, req.body);
+        res.status(204).json(user)
     }
-    res.send('USER NOT FOUND');
+    res.status(404).send('user not found');
 });
 
 app.delete('/user/:id', (req, res) => {
     const id = req.params.id;
     const user = getCurrentUser(id);
     if (user) {
-        user.isDeleted = true;
+        deleteUser(user);
         res.send(`user ${user.id} successfully deleted`);
     }
-    res.send('USER NOT FOUND');
+    res.status(404).send('user not found');
 });
 
 app.listen(3000, function () {
--- restAPI/src/utils/common.ts
+++ restAPI/src/utils/common.ts
@@ -1,5 +1,6 @@
 import { UserModel } from "./user.model";
 import { v4 as uuidv4 } from "uuid";
+import { User } from "./user";
 
 export const users: UserModel[] = [{
     id: uuidv4(),
@@ -23,13 +24,23 @@ export const users: UserModel[] = [{
 
 export const getAutoSuggestUsers = (loginSubstring: string, limit: number): UserModel[] => {
     return users
-        .map(u => u)
         .sort((a, b) => a.login > b.login ? 1 : -1)
         .filter(user => user.login.indexOf(loginSubstring) > -1)
-        .filter((_, index) => index < limit)
+        .slice(0, limit)
 };
 
-export const activeUsers = users.filter(user => !user.isDeleted);
+export const getActiveUsers = () => users.filter(user => !user.isDeleted);
 export const getCurrentUser = (id: string) => users.find(user => user.id === id);
+export const createUser = (body: any) => {
+    const {login, password, age} = body;
+    return new User(login, password, age);
+};
 export const addUser = (user: UserModel) => users.push(user);
+export const deleteUser = (user: UserModel) => user.isDeleted = true;
+export const updateUser = (user: UserModel, body: any) => {
+    const {login, password, age} = body;
+    user.login = login || user.login;
+    user.password = password || user.password;
+    user.age = age || user.age;
+};
 export const green = (text: string | number) => `\x1b[32m${text}\x1b[0m`;
