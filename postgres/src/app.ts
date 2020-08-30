1f7fbf8581cb582db6515009b588268c9d8663a0
Viktar Belski <belski.viktor.dev@gmail.com>
2020-08-30 12:05:31 +0300

    structure

--- postgres/src/app.ts
+++ postgres/src/app.ts
@@ -1,13 +1,12 @@
 import express from "express";
+import router from "./controllers";
 
 const PORT = Number(process.env.PORT) || 3000;
 const app: express.Application = express();
 
 app.use(express.json());
 
-app.get('/', (req, res) =>{
-    res.send("hello world!")
-});
+app.use(router);
 
 app.listen(PORT, function () {
     console.log(`App is listening on port: ${PORT}!`);
