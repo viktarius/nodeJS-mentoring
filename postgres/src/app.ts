cb2f913a85e892e133d3a0d4d49ad15cb9fcc5e6
Viktar Belski <belski.viktor.dev@gmail.com>
2020-09-27 00:38:34 +0300

    task 2: error middleware

--- postgres/src/app.ts
+++ postgres/src/app.ts
@@ -3,6 +3,7 @@ import router from "./controllers";
 import { port } from "./config/app.config";
 import { initData, initDB } from "./core/services/database.service";
 import { errorMiddleware, infoLoggerMiddleware } from "./core/middleware";
+import { logger } from "./core/logger";
 
 const app: express.Application = express();
 
@@ -14,11 +15,11 @@ app.use(router);
 
 app.use(errorMiddleware);
 
-// process.on('uncaughtException', (err) => {
-//     logger.log('error',"[Inside 'uncaughtException' event]" + err.stack || err.message);
-// }).on('unhandledRejection', (reason, p) => {
-//     logger.log('error',reason + 'Unhandled Rejection at Promise' + p);
-// });
+process.on('uncaughtException', (err) => {
+    logger.error("[Inside 'uncaughtException' event]" + err.stack || err.message);
+}).on('unhandledRejection', (reason, p) => {
+    logger.error(reason + 'Unhandled Rejection at Promise' + p);
+});
 
 app.listen(port, async () => {
     await initDB();
--- postgres/src/core/middleware/error.middleware.ts
+++ postgres/src/core/middleware/error.middleware.ts
@@ -1,9 +1,10 @@
 import { NextFunction, Request, Response } from 'express';
 import { HttpException } from "../exeption";
+import { logger } from "../logger";
 
 export const errorMiddleware = (error: HttpException, request: Request, response: Response, next: NextFunction) => {
     const status = error.status || 500;
     const message = error.message || 'Something went wrong';
-    // logger.log('error', `${status} : ${message}`);
+    logger.error(`${status}: ${message}`);
     response.status(status).send({status, message})
 };
